487204db24f0c91192adb72b0113e61b85500b42
Xwaffle1 <chasecool56@gmail.com>
2023-04-06 23:35:27 -0400

    Fix check for Game node.

--- ParallaxBackground.gd
+++ ParallaxBackground.gd
@@ -2,7 +2,7 @@ extends ParallaxBackground
 
 
 func _process(delta):
-	if get_tree().root.get_node("Game") != null:
+	if get_tree().root.has_node("Game") != null:
 		if !get_tree().root.get_node("Game").is_game_over and get_tree().root.get_node("Game").is_game_started:
 			$ParallaxLayer.motion_offset.x += -60 * delta
 	else:
